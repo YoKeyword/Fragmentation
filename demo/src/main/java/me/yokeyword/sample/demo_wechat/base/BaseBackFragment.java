ae22bbc03160032cdd77f6c6f12a9458ce1cc0a6
王右右 <328903522@qq.com>
2017-04-16 20:57:30 +0800

    Add a sample of setParallaxOffset().

--- demo/src/main/java/me/yokeyword/sample/demo_wechat/base/BaseBackFragment.java
+++ demo/src/main/java/me/yokeyword/sample/demo_wechat/base/BaseBackFragment.java
@@ -1,5 +1,7 @@
 package me.yokeyword.sample.demo_wechat.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -11,6 +13,12 @@ import me.yokeyword.sample.R;
  */
 public class BaseBackFragment extends SwipeBackFragment {
     private static final String TAG = "Fragmentation";
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setParallaxOffset(0.5f);
+    }
 
     protected void initToolbarNav(Toolbar toolbar) {
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
