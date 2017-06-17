2eb613db94c57fd78e485e6fbfa8e0f2c03663d6
王右右 <328903522@qq.com>
2017-06-17 18:07:00 +0800

    Compat support-25.4.0+

--- fragmentation/src/main/java/android/support/v4/app/FragmentTransactionBugFixHack.java
+++ fragmentation/src/main/java/android/support/v4/app/FragmentTransactionBugFixHack.java
@@ -1,22 +1,36 @@
 package android.support.v4.app;
 
 
+import java.lang.reflect.Field;
 import java.util.Collections;
 
 /**
  * http://stackoverflow.com/questions/23504790/android-multiple-fragment-transaction-ordering
  */
 public class FragmentTransactionBugFixHack {
+    private static boolean mNeedHack;
+
+    static {
+        Field[] fields = FragmentManagerImpl.class.getDeclaredFields();
+        boolean supportLessThan25dot4 = false;
+        for (Field field : fields) {
+            if (field.getName().equals("mAvailIndices")) {
+                supportLessThan25dot4 = true;
+                break;
+            }
+        }
+        mNeedHack = supportLessThan25dot4;
+    }
 
     public static void reorderIndices(FragmentManager fragmentManager) {
+        if (!mNeedHack) return;
+
         if (!(fragmentManager instanceof FragmentManagerImpl))
             return;
         try {
             FragmentManagerImpl fragmentManagerImpl = (FragmentManagerImpl) fragmentManager;
             if (fragmentManagerImpl.mAvailIndices != null && fragmentManagerImpl.mAvailIndices.size() > 1) {
-//                System.out.println("排序前-->" + fragmentManagerImpl.mAvailIndices);
                 Collections.sort(fragmentManagerImpl.mAvailIndices, Collections.reverseOrder());
-//                System.out.println("排序后-->" + fragmentManagerImpl.mAvailIndices);
             }
         } catch (Exception e) {
             e.printStackTrace();
