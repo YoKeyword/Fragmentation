1798d287b8e4835ff6744bf2242087764375789b
王右右 <328903522@qq.com>
2017-07-10 20:56:49 +0800

    Optimize annotations.

--- demo/src/main/java/me/yokeyword/sample/App.java
+++ demo/src/main/java/me/yokeyword/sample/App.java
@@ -15,18 +15,17 @@ public class App extends Application {
         super.onCreate();
 
         Fragmentation.builder()
-                // 设置 栈视图 模式为 悬浮球模式   SHAKE: 摇一摇唤出  默认NONE：隐藏
+                // 设置 栈视图 模式为 悬浮球模式   SHAKE: 摇一摇唤出  默认NONE：隐藏， 仅在Debug环境生效
                 .stackViewMode(Fragmentation.BUBBLE)
-                // true时，遇到异常："Can not perform this action after onSaveInstanceState!"时，会抛出;
-                // false时，不会抛出，会捕获，可以在handleException()里监听到
-                // 同时，如果为false, stackViewMode为None
+                // 开发环境：true时，遇到异常："Can not perform this action after onSaveInstanceState!"时，抛出，并Crash;
+                // 生产环境：false时，不抛出，不会Crash，会捕获，可以在handleException()里监听到
                 .debug(true) // 实际场景建议.debug(BuildConfig.DEBUG)
-                // 线上环境时，可能会遇到上述异常，此时debug=false，不会抛出该异常（避免crash），会捕获
-                // 建议在回调处上传至我们的Crash检测服务器
+                // 生产环境时，捕获上述异常（避免crash），会捕获
+                // 建议在回调处上传下面异常到崩溃监控服务器
                 .handleException(new ExceptionHandler() {
                     @Override
                     public void onException(Exception e) {
-                        // 以Bugtags为例子: 手动把捕获到的 Exception 传到 Bugtags 后台。
+                        // 以Bugtags为例子: 把捕获到的 Exception 传到 Bugtags 后台。
                         // Bugtags.sendException(e);
                     }
                 })
