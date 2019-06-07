751d5ac87c6cc24b92a36d1e7ebb2631877eb80b
YoKey <yokeyword@gmail.com>
2019-06-07 16:42:13 +0800

    _mActivity: FragmentActivity -> SupportActivity

--- fragmentation/src/main/java/me/yokeyword/fragmentation/SupportFragment.java
+++ fragmentation/src/main/java/me/yokeyword/fragmentation/SupportFragment.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.animation.Animation;
 
@@ -19,7 +18,7 @@ import me.yokeyword.fragmentation.anim.FragmentAnimator;
  */
 public class SupportFragment extends Fragment implements ISupportFragment {
     final SupportFragmentDelegate mDelegate = new SupportFragmentDelegate(this);
-    protected FragmentActivity _mActivity;
+    protected SupportActivity _mActivity;
 
     @Override
     public SupportFragmentDelegate getSupportDelegate() {
@@ -39,7 +38,7 @@ public class SupportFragment extends Fragment implements ISupportFragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         mDelegate.onAttach(activity);
-        _mActivity = mDelegate.getActivity();
+        _mActivity = (SupportActivity) mDelegate.getActivity();
     }
 
     @Override
